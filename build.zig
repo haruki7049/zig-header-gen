5fe144b4d3d6677f1dd0b717691cb9bbb53bf26e
haruki7049 <tontonkirikiri@gmail.com>
2025-01-06 00:27:47 +0900

    chore!(header_gen.zig): Rename to root.zig

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const header_gen = b.addModule("header_gen", .{ .root_source_file = b.path("src/header_gen.zig"), });
+    const header_gen = b.addModule("header_gen", .{ .root_source_file = b.path("src/root.zig"), });
     const example = b.addExecutable(.{
         .name = "example",
         .root_source_file = b.path("example/main.zig"),
